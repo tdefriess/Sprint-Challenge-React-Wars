8b923a7774342af2717af86516f40b990f3ae4ae
Tyler de Friess <tyler.defriess@gmail.com>
2020-01-24 13:45:38 -0500

    Attempted pagination stretch goal.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Axios from 'axios';
 import CharacterList from "./components/CharacterList";
+import { Button } from "reactstrap";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -11,12 +12,25 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   const [characters, setCharacters] = useState([]);
+  const [url, setUrl] = useState("https://swapi.co/api/people/");
+  const [next, setNext] = useState();
+  const [prev, setPrev] = useState();
+
+  // const getCharacters = (link) => {
+  //   setUrl(link);
+  //   return
+  // }
+   
 
   useEffect(() => {
     Axios
-      .get("https://swapi.co/api/people/")
+      .get(url)
       .then(response => {
-        setCharacters(response.data.results);        
+        console.log(response);
+        setCharacters(response.data.results);
+        setNext(response.data.next);
+        setPrev(response.data.previous);
+        console.log(next, prev);
       })
       .catch(err => {
         console.log("The data was not returned", err);
@@ -24,9 +38,12 @@ const App = () => {
   }, []);
 
 
+
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      {/* <Button color="primary" onClick={getCharacters(prev)}>Prev</Button>
+      <Button color="primary" onClick={getCharacters(next)}>Next</Button> */}
       <CharacterList characters={characters} setChars={setCharacters}/>
     </div>
   );
